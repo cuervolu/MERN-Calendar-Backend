7f1c4e48a5e8b2837ab4a063c080beb012298b6e
cuervolu <cuervolu@protonmail.com>
2023-01-01 23:48:13 -0300

    fix: corrección de error en ruta

--- index.js
+++ index.js
@@ -20,7 +20,11 @@ app.use(express.json());
 
 //Rutas
 app.use('/api/auth',require('./routes/auth'));
-app.use('/api/events',require('./routes/events'));
+
+
+app.get('*', (req, res) => {
+    res.sendFile(__dirname + '/public/index.html');
+})
 
 //Escuchar peticiones
 app.listen(process.env.PORT, () => {
